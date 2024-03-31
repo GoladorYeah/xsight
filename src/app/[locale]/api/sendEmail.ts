8725b913ea92beee14312786e5085d8f335561a7
DispeL <dispel063@gmail.com>
2024-03-31 16:14:54 +0200

    ggggggg

--- src/app/[locale]/api/sendEmail.ts
+++ src/app/[locale]/api/sendEmail.ts
@@ -1,7 +1,7 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import nodemailer from 'nodemailer';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const {to, subject, text} = req.body;
 
