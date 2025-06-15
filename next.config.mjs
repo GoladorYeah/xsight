53a955217a853fa5fa319f2d6000f8661fe3cc29
GoladorYeah <dispel063@gmail.com>
2025-06-15 23:24:29 +0200

    add ignored eslint

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const withNextIntl = createNextIntlPlugin(
 );
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
